3215b0f19193766823be57e8d2c6882eb9344550
unknown <Santo Tomas@SCL076PCL8023.alumnos.cst>
2025-11-07 12:35:47 -0300

    Inicio

---
+++ src/main/java/Modelo/Usuario.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Santo Tomas
+ */
+public class Usuario {
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private String rut;
+    private String correo;
+    private String roles;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String nombre, String apellido, String rut, String correo, String roles) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.rut = rut;
+        this.correo = correo;
+        this.roles = roles;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+    
+    
+}
